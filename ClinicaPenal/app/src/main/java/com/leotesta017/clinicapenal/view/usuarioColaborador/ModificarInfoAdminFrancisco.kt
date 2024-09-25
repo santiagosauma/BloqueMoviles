d5225a5a8727c4ca1e6c961a1f42fea7b9f36e03
FranciscoVaqueraSaavedra <141375999+FranciscoVaqueraSaavedra@users.noreply.github.com>
2024-09-24 20:42:45 -0600

    Add files via upload

---
+++ ClinicaPenal/app/src/main/java/com/leotesta017/clinicapenal/view/usuarioColaborador/ModificarInfoAdminFrancisco.kt
@@ -0,0 +1,79 @@
+package com.leotesta017.clinicapenal.view.usuarioColaborador
+
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.FormatListBulleted
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.FormatBold
+import androidx.compose.material.icons.filled.FormatItalic
+import androidx.compose.material.icons.filled.Save
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.leotesta017.clinicapenal.view.funcionesDeUsoGeneral.AdminBarraNav
+import com.leotesta017.clinicapenal.view.funcionesDeUsoGeneral.RoundedButton
+import com.leotesta017.clinicapenal.view.funcionesDeUsoGeneral.TopBar
+
+// PANTALLA TEMPLATE
+
+import com.leotesta017.clinicapenal.view.templatesPantallas.PantallaModificarInfo
+
+// FUNCIONES GENERICAS
+
+
+// VIEWMODEL
+import com.leotesta017.clinicapenal.viewmodel.CategoryViewModel
+import com.leotesta017.clinicapenal.viewmodel.ServicioViewModel
+import com.leotesta017.clinicapenal.viewmodel.VideoViewModel
+
+@Composable
+fun ModificarInfoAdminR(navController: NavController? ){
+    PantallaModificarInfo(
+
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewModificarInfoAdminR(){
+    ModificarInfoAdminR(navController = null)
+}
