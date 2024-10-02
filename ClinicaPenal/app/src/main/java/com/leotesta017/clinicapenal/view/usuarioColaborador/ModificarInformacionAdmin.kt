cc646681a7f78d626e611fca90c17e84e7301227
SantiagoSauma <lssaumap@gmail.com>
2024-10-02 11:39:02 -0600

    Created SnackBar when UPDATE is successful

--- ClinicaPenal/app/src/main/java/com/leotesta017/clinicapenal/view/usuarioColaborador/ModificarInformacionAdmin.kt
+++ ClinicaPenal/app/src/main/java/com/leotesta017/clinicapenal/view/usuarioColaborador/ModificarInformacionAdmin.kt
@@ -1,14 +1,22 @@
 package com.leotesta017.clinicapenal.view.usuarioColaborador
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -32,6 +40,10 @@ fun ModificarInfoAdmin(
     viewModel: CategoryViewModel = viewModel()
 ) {
     val nombre = titulo
+    val scope = rememberCoroutineScope()
+
+    val snackbarHostState = remember { SnackbarHostState() }
+    var snackbarMessage by remember { mutableStateOf("") }
 
     LaunchedEffect(id) {
         viewModel.fetchContenidoById(id)
@@ -44,45 +56,65 @@ fun ModificarInfoAdmin(
         .replace("-", "\n-")
         .replace(Regex("\\*\\*(.*?)\\*\\*"), "\n**$1**\n") // Colocar las negritas en nuevas líneas
 
-    when {
-        contenido.isEmpty() && error == null -> {
-            Column(modifier = Modifier.fillMaxSize()) {
-                CircularProgressIndicator(modifier = Modifier.align(Alignment.CenterHorizontally))
+    Box(modifier = Modifier.fillMaxSize()) {
+        when {
+            contenido.isEmpty() && error == null -> {
+                Column(modifier = Modifier.fillMaxSize()) {
+                    CircularProgressIndicator(modifier = Modifier.align(Alignment.CenterHorizontally))
+                }
+            }
+            error != null -> {
+                Text(text = error ?: "Error desconocido", color = Color.Red)
+            }
+            contenido.isNotEmpty() -> {
+                ModificarInfoTemplate(
+                    navController = navController,
+                    titulo = "Modificar Categoria",
+                    initialName = nombre,
+                    initialDescription = descripcion,
+                    id = id,
+                    contenido = formattedContent,
+                    urlimagen = urlimagen,
+                    bottomBarContent = {
+                        AdminBarraNav(navController = navController, modifier = Modifier.fillMaxWidth())
+                    },
+                    onSaveClick = { nombre, descripcion, url_imagen, textContent ->
+                        val contentToSave = textContent
+                            .replace("\n-", " -")
+                            .replace("\n", " ")
+                            .replace(Regex("\\s{2,}"), " ")
+
+                        CoroutineScope(Dispatchers.IO).launch {
+                            try {
+                                val modCategoria = Categoria(
+                                    id = id,
+                                    titulo = nombre,
+                                    descripcion = descripcion,
+                                    contenido = contentToSave,
+                                    url_imagen = url_imagen
+                                )
+                                viewModel.updateCategoria(modCategoria)
+                                snackbarMessage = "Contenido actualizado con éxito"
+                                scope.launch { snackbarHostState.showSnackbar(snackbarMessage) }
+                            } catch (e: Exception) {
+                                snackbarMessage = "Error al actualizar el contenido"
+                                scope.launch { snackbarHostState.showSnackbar(snackbarMessage) }
+                            }
+                        }
+                    },
+                    onCancelClick = {}
+                )
             }
         }
-        error != null -> {
-            Text(text = error ?: "Error desconocido", color = Color.Red)
-        }
-        contenido.isNotEmpty() -> {
-            ModificarInfoTemplate(
-                navController = navController,
-                titulo = "Modificar Categoria",
-                initialName = nombre,
-                initialDescription = descripcion,
-                id = id,
-                contenido = formattedContent, // Pasamos el contenido formateado a la plantilla
-                urlimagen = urlimagen,
-                bottomBarContent = {
-                    AdminBarraNav(navController = navController, modifier = Modifier.fillMaxWidth())
-                },
-                onSaveClick = { nombre, descripcion, url_imagen, textContent ->
-                    val contentToSave = textContent
-                        .replace("\n-", " -")
-                        .replace("\n", " ")
-                        .replace(Regex("\\s{2,}"), " ")
 
-                    CoroutineScope(Dispatchers.IO).launch {
-                        val modCategoria = Categoria(
-                            id = id,
-                            titulo = nombre,
-                            descripcion = descripcion,
-                            contenido = contentToSave,
-                            url_imagen = url_imagen
-                        )
-                        viewModel.updateCategoria(modCategoria)
-                    }
-                },
-                onCancelClick = {}
+        SnackbarHost(
+            hostState = snackbarHostState,
+            modifier = Modifier.align(Alignment.BottomCenter)
+        ) {
+            Snackbar(
+                snackbarData = it,
+                containerColor = Color(0xFF303665),
+                contentColor = Color.White
             )
         }
     }
